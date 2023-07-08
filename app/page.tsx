e6b79b08b43b202bc92aeeac583103b5c00b846b
Nobuo Kato <katonobu4649@gmail.com>
2023-07-08 17:29:37 +0900

    カスタムページを追加した。

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import dynamic from 'next/dynamic'
 import {Resource} from "react-admin";
 import serialPorts from "./serialPorts/src/"
 import webSerialProvider from "./webSerialDataProvider/src";
+import CustomRouteSample from "./CustomRouteSample/src"
+import { CustomRoutes } from 'react-admin';
+import { Route } from "react-router-dom";
 
 const Admin = dynamic(() => import('react-admin').then((module)=>module.Admin), {
   ssr: false, // サーバーサイドレンダリングを無効化
@@ -15,6 +18,9 @@ const App = () => (
     dataProvider={serialDataProvider}
   >
     <Resource name="webserialport" {...serialPorts} />
+    <CustomRoutes>
+      <Route path="/customRouteSample" element={<CustomRouteSample />} />
+    </CustomRoutes>    
   </Admin>
 );
 
