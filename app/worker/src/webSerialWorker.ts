72875da1f443b7b6acf3841ed84d963f725bb8c9
Nobuo Kato <katonobu4649@gmail.com>
2023-08-22 12:34:29 +0900

    delete worker interval log output

--- app/worker/src/webSerialWorker.ts
+++ app/worker/src/webSerialWorker.ts
@@ -3,5 +3,5 @@ import workerHandler from './workerHandler'
 (()=>{
     console.log("WebSerialWorkerStarted")
     workerHandler()
-    setInterval(()=>console.log("a"), 10 * 1000)
+//    setInterval(()=>console.log("a"), 10 * 1000)
 })()
